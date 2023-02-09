4d66983a35045dae69f5e26c749126507d2f5528
Abigail O'Meara <aomeara@smith.edu>
2023-02-08 20:59:51 -0500

    upload

--- computer.py
+++ computer.py
@@ -1,10 +1,41 @@
-class Computer:
+class Computer: # think about a blueprint 
 
     # What attributes will it need?
-
+    description: str
+    processor_type: str
+    hard_drive_capacity: int
+    memory: int
+    operating_system: str
+    year_made: int
+    price: int
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, description, processor_type, hard_drive_capacity, memory, operating_system, year_made, price):
+        self.description = description
+        self.processor_type = processor_type
+        self.hard_drive_capacity = hard_drive_capacity
+        self.memory = memory
+        self.operating_system = operating_system
+        self.year_made = year_made
+        self.price = price
 
     # What methods will you need?
+    def printDetails(self):
+        print(self.description)
+        print(self.processor_type)
+        print(self.hard_drive_capacity)
+        print(self.memory)
+        print(self.operating_system)
+        print(self.year_made)
+        print(self.price)
+
+def main():
+    c = Computer("MacBook Pro 13inch", "Processor 4", 400, 60, "Memory 2", 2020, 1499.89)
+    c.printDetails()
+
+main()
+
+
+    
+        
+        
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,58 @@
+from computer import *
+# potential constraint 
+
 class ResaleShop:
+# have blueprint, now need an inventory 
 
     # What attributes will it need?
-
+    inventory = []
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self):
+        self.inventory = [] 
+        #now have space and memory in the list, object are the computers 
+        # always starts as an empty list, can add and remove to it 
+
+#  buy, update_price, sell, print_inventory, refurbish
 
     # What methods will you need?
+    # lists (not dictionary)
+    
+        #call computer (...) constructor
+        # to create new computer instance
+
+        #call inventory.append(...) to add the
+        # new computer instance to the inventory
+   
+    def buy(self, c:Computer):
+        self.inventory.append(c)
+        # this buys a new computer, adds to the inventory
+    
+    #def new_computer(self):
+        #self.inventory.append(c)
+        # this coding is repeditive of buy
+
+    def sell(self, c:Computer):
+        if c in self.inventory: 
+            self.inventory.remove(c)
+        else: print("computer not in inventory")
+        # this sells a computer if found in the inventory, if not in inventory it prints that it was not found
+
+    def update_price(self, c:Computer, new_price: int):
+        if c in self.inventory: 
+            new_price: int
+            c.price = new_price
+        else:
+            print("Item", c, "not found. Cannot update price.")
+        # this finds the computer, then updates the price given a new int price, and will replace the old price given 
+
+    def refurbish(self, c: Computer, operating_system_new: str):
+        c.operating_system = operating_system_new
+        # this finds the computer, then updates the operating system
+
+    def print_inventory(self):
+        for c in self.inventory:
+            c.printDetails()
+        # this prints the new inventory after all attributes 
+
+main()
